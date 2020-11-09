275ac8509a11594bbc8e59a2889b9fc206f50af2
Davi Yan <74159136+daviyan5@users.noreply.github.com>
2020-11-09 15:53:18 -0300

    Identação ruim da gota nos antigos ave.

--- pasta/pokedex.c
+++ pasta/pokedex.c
@@ -3,12 +3,11 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() 
-{
-int hora,minutos;
-scanf("%d %d",&hora,&minutos);
-if (hora==0) printf("12\n%02d\nam\n",minutos);
-else if (hora<12) printf("%02d\n%02d\nam\n",hora,minutos);
-else if (hora>12) printf("%02d\n%02d\npm\n",(hora-12),minutos);
-else if (hora==12) printf("12\n%02d\npm\n",minutos);
+int main() {
+  int hora,minutos;
+  scanf("%d %d",&hora,&minutos);
+  if (hora==0) printf("12\n%02d\nam\n",minutos);
+  else if (hora<12) printf("%02d\n%02d\nam\n",hora,minutos);
+  else if (hora>12) printf("%02d\n%02d\npm\n",(hora-12),minutos);
+  else if (hora==12) printf("12\n%02d\npm\n",minutos);
 }
